e237353a5f8aeefc3199d5c135862291a5d0e8be
zhangrao1990 <zhangrao1990@gmail.com>
2017-03-14 23:33:51 -0700

    Add file2pair function
    generate pairs in the form
    [[authorX, [paras], [authorY, paras]]
    default parameter are 0.2 and 2000

--- utils/file2list3.py
+++ utils/file2list3.py
@@ -3,6 +3,7 @@ import re
 import random
 from unidecode import unidecode
 
+n_class = 50
 sent_len = 10       # each paragraph should contain at least 5 sentences
 para_len = 300      # each author should contain at least 300 paragraphs
 
@@ -30,7 +31,10 @@ def read_mydir(input_dir):
             author_newss_dict[author] = news_list
     return author_newss_dict
 
-
+"""
+input: data set directory
+output: single of {author: [list of story paras]}
+"""
 def file2list(input_dir):
     auth_para_list = []
     auth_newss_dict = read_mydir(input_dir)
@@ -81,12 +85,48 @@ def file2list(input_dir):
             print "WARNING: this author --", author, "does not meet the paragraph requirement"
     return auth_para_list
 
+"""
+input: data set directory
+output: pair of {author: [list of story paras]}
+"""
+def file2pair(input_path, self_ratio = 0.2, sample_num = 2000):
+    auth_para_list = file2list(input_path)
+    total_para = n_class * para_len
+    pair_list = []
+    for ii in xrange(n_class):
+        # self, self
+        lo = ii * para_len
+        hi = (ii + 1) * para_len - 1
+        pair_elem = []
+        for jj in xrange(int(self_ratio * sample_num)):
+            idx1 = random.randint(lo, hi)
+            idx2 = random.randint(lo, hi)
+            pair_elem.append([auth_para_list[idx1], auth_para_list[idx2]])
+        # self, other
+        for kk in xrange(int((1 - self_ratio) * sample_num)):
+            idx3 = random.randint(lo, hi)
+            low = (0 + ii * para_len)
+            high = (total_para - para_len - 1 + ii * para_len)
+            idx4 = random.randint(low, high) % total_para
+            pair_elem.append([auth_para_list[idx3], auth_para_list[idx4]])
+        pair_list.extend(pair_elem)
+    return pair_list
+
+
+
 if __name__=='__main__':
     cwd = os.getcwd()
     test_path = cwd + '/../dataset/gutenberg'
-    res = file2list(test_path)
 
+    res = file2list(test_path)
     print len(res)
     for ii in xrange(len(res)):
         if ii % 100 == 0:
             print res[ii]
+
+    print "above are single dogs, below are couple dogs"
+    pair_list = file2pair(test_path)
+    print len(pair_list)
+    for ii in xrange(len(pair_list)):
+        if ii % 1000 == 0:
+            print pair_list[ii]
